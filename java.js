f876cc636061a35f52bc165c845b2c2b4f427c0f
oluwaseun2017 <oluwaseun.learning@gmail.com>
2025-10-04 12:34:21 -0700

    third assignment

---
+++ java.js
@@ -0,0 +1,12 @@
+// const book = {}
+
+// book.title = 'The country',
+// book.description = 'The tribe',
+// book.numberofpages = '4'
+// book.authour = 'Tinubu',
+// book.isNigeria = true
+// console.log(book)
+
+const book = {title:'The country', description:'The Tribe', numberofpages: 4, authour:'Tinubu',isNigeria:'True'}
+
+console.log(book)
